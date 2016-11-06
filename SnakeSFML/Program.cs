d796aa4a72a15411417ae79c56c651fd0c796bf6
matyjb <matyjasiakbartosz@gmail.com>
2016-11-06 22:11:00 +0100

    Made moving of one snake particle with WASD keys

--- SnakeSFML/Program.cs
+++ SnakeSFML/Program.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using SFML;
 using SFML.Graphics;
 using SFML.Window;
+
+using Snake;
+
 namespace SnakeSFML
 {
     class Program
     {
+        static void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+            
+        }
+
+        static void MoveSnake(Keyboard.Key key, object window, object snakepart)
+        {
+            SnakeParticle snkprt = (SnakeParticle)snakepart;
+
+            if (key == Keyboard.Key.W)
+                snkprt.MoveBy(0, 20);
+
+            else if (key == Keyboard.Key.S)
+                snkprt.MoveBy(0, -20);
+
+            else if (key == Keyboard.Key.A)
+                snkprt.MoveBy(20, 0);
+
+            else if (key == Keyboard.Key.D)
+                snkprt.MoveBy(-20, 0);
+        }
+
         static void Main()
         {
-            RenderWindow
+            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "SnakeSFML");
+            window.Closed += new EventHandler(OnClose);
+            
+            RenderTexture texture = new RenderTexture(20, 20);
+            texture.Clear(Color.White);
+
+            SnakeParticle snakepart = new SnakeParticle(texture.Texture, 20, 20);
+
+            window.KeyPressed += (o, a) => MoveSnake(a.Code, window, snakepart);
+            //window.KeyReleased += (o, a) => MoveSnake(a.Code, window, snakepart);
+
+            snakepart.Draw(window);
+
+            while (window.IsOpen)
+            {
+                window.Clear(Color.Black);
+                snakepart.Draw(window);
+                window.DispatchEvents();
+                window.Display();
+                
+            }
         }
     }
 }
---
+++ SnakeSFML/Snake.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.System;
+using SFML.Graphics;
+
+namespace Snake
+{
+
+
+    class SnakeParticle
+    {
+        Sprite Part = new Sprite();
+
+        public SnakeParticle(Texture texture, float x = 0, float y = 0)
+        {
+            Part.Texture = texture;
+            Part.Position = new Vector2f(x,y);
+        }
+
+        public void MoveBy(float x, float y)
+        {
+            Part.Position = new Vector2f(Part.Position.X - x, Part.Position.Y - y);
+            Console.WriteLine("Poruszono");
+        }
+
+        public void MoveTo(float x, float y)
+        {
+            Part.Position = new Vector2f(x, y);
+        }
+
+        public void Draw(object window)
+        {
+            RenderWindow windoww = (RenderWindow)window;
+            windoww.Draw(Part);
+        }
+    }
+
+}
