d797dfb8b98e502207482dce027ecf1d442d1457
matyjb <matyjasiakbartosz@gmail.com>
2016-11-06 18:51:22 +0100

    initialized project

---
+++ SnakeSFML/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeSFML
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
