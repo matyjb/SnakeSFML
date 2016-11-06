ffd8f03896dbbacc955b1ce0a933aa1a8db2a286
matyjb <matyjasiakbartosz@gmail.com>
2016-11-06 19:18:24 +0100

    set up SFML in solution/project

--- SnakeSFML/Program.cs
+++ SnakeSFML/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
 namespace SnakeSFML
 {
     class Program
     {
         static void Main()
         {
-
+            RenderWindow
         }
     }
 }
