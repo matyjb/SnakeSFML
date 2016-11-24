d52c62fff2833b94c8a70b339fe2a37feb44b11e
matyjb <matyjasiakbartosz@gmail.com>
2016-11-24 19:10:28 +0100

    Added simple points particles

---
+++ SnakeSFML/PointsAnim.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SnakeSFML
+{
+    class PointAnim
+    {
+        public Sprite PAnim = new Sprite();
+        public uint state = 1;
+
+        public PointAnim(Texture texture, float x = 0, float y = 0, uint statee = 1)
+        {
+            
+            PAnim.Texture = texture;
+            PAnim.Position = new Vector2f(x, y);
+            state = statee;
+            PAnim.Origin = new Vector2f(5, 5);
+        }
+        public void MoveBy(float x, float y)
+        {
+            PAnim.Position = new Vector2f(PAnim.Position.X + x, PAnim.Position.Y + y);
+            Console.WriteLine("PAnim moved by x = " + x + " | y = " + y);
+        }
+
+        public void MoveTo(float x, float y)
+        {
+            PAnim.Position = new Vector2f(x, y);
+            Console.WriteLine("PAnim moved to x = " + x + " | y = " + y);
+        }
+
+        public void Draw(object window)
+        {
+            RenderWindow windoww = (RenderWindow)window;
+
+            
+            PAnim.Position = PAnim.Position + new Vector2f(-0.02f, -0.02f);
+            windoww.Draw(PAnim);
+            PAnim.Position = PAnim.Position + new Vector2f(0.04f * state, 0);
+            windoww.Draw(PAnim);
+            PAnim.Position = PAnim.Position + new Vector2f(0, 0.04f * state);
+            windoww.Draw(PAnim);
+            PAnim.Position = PAnim.Position + new Vector2f(-0.04f * state, 0);
+            windoww.Draw(PAnim);
+            PAnim.Position = PAnim.Position + new Vector2f(0,-0.04f * state);
+          
+        }
+
+        public float GetX()
+        {
+            return PAnim.Position.X;
+        }
+
+        public float GetY()
+        {
+            return PAnim.Position.Y;
+        }
+        
+    }
+
+    
+}
--- SnakeSFML/Program.cs
+++ SnakeSFML/Program.cs
@@ -18,19 +18,20 @@ namespace SnakeSFML
     {
         private static RenderWindow window;
         private static SnakeDirection direction;
-        private static RenderTexture textureW, textureR;
+        private static RenderTexture textureW, textureR, textureSR;
         private static List<SnakeParticle> snakePartsList;
+        private static List<PointAnim> pointsAnims;
         private static bool canStillChangeDirection;
         private static bool removeParticle = true;
         private static PointToEat.PointToEat pointToEat;
-        private static bool MainGameOn=true;
+        private static bool MainGameOn = true;
         private static Font arialF = new Font("arial.ttf");
 
         static void OnClose(object sender, EventArgs e)
         {
             RenderWindow window = (RenderWindow)sender;
             window.Close();
-            
+
         }
 
         static void SnakeAddParticleToList(Texture texture, float x, float y)
@@ -41,33 +42,33 @@ namespace SnakeSFML
 
         static void MoveSnake(bool removeLastParticle)
         {
-            if(direction.direction == Directions.N)
+            if (direction.direction == Directions.N)
             {
                 snakePartsList.Insert(0, new SnakeParticle(textureW.Texture, snakePartsList[0].GetX(), snakePartsList[0].GetY() - 20));
-                
-                Console.WriteLine("Moved up");
+
+                //Console.WriteLine("Moved up");
             }
             else if (direction.direction == Directions.S)
             {
                 snakePartsList.Insert(0, new SnakeParticle(textureW.Texture, snakePartsList[0].GetX(), snakePartsList[0].GetY() + 20));
-                
-                Console.WriteLine("Moved down");
+
+                //Console.WriteLine("Moved down");
             }
 
             else if (direction.direction == Directions.W)
             {
                 snakePartsList.Insert(0, new SnakeParticle(textureW.Texture, snakePartsList[0].GetX() - 20, snakePartsList[0].GetY()));
-                
-                Console.WriteLine("Moved left");
+
+                //Console.WriteLine("Moved left");
             }
 
             else if (direction.direction == Directions.E)
             {
                 snakePartsList.Insert(0, new SnakeParticle(textureW.Texture, snakePartsList[0].GetX() + 20, snakePartsList[0].GetY()));
-                
-                Console.WriteLine("Moved right");
+
+                //Console.WriteLine("Moved right");
             }
-            if(removeLastParticle)
+            if (removeLastParticle)
                 snakePartsList.Remove(snakePartsList[snakePartsList.Count - 1]);
         }
 
@@ -106,7 +107,7 @@ namespace SnakeSFML
                         canStillChangeDirection = false;
                     }
                 }
-            }  
+            }
             if (key.Code.Equals(Keyboard.Key.Escape))
             {
                 OnClose(window, null);
@@ -115,12 +116,12 @@ namespace SnakeSFML
             {
                 removeParticle = false;
             }
-                      
+
         }
 
         static bool CheckPointPick()
         {
-            if(snakePartsList[0].GetX().Equals(pointToEat.GetX()) && snakePartsList[0].GetY().Equals(pointToEat.GetY()))
+            if (snakePartsList[0].GetX().Equals(pointToEat.GetX()) && snakePartsList[0].GetY().Equals(pointToEat.GetY()))
             {
                 return true;
             }
@@ -131,7 +132,7 @@ namespace SnakeSFML
         {
             Random rndX = new Random(); Random rndY = new Random();
             float modX = 1, modY = 1;
-            bool isPointNotOnParticle=true;
+            bool isPointNotOnParticle = true;
             do
             {
                 modX = rndX.Next(0, Convert.ToInt16(window.Size.X) / 20) * 20;
@@ -161,27 +162,48 @@ namespace SnakeSFML
                 return true;
 
             return false;
-        }   
-        
+        }
+
+        static void DrawPointsAnim()
+        {
+            foreach (PointAnim pA in pointsAnims)
+            {
+                pA.Draw(window);
+                pA.state++;
+                pA.PAnim.Rotation += 1;
+            }
+            int i = 0;
+            while (i < pointsAnims.Count)
+            {
+                if (pointsAnims[i].state == int.MaxValue)
+                {
+                    pointsAnims.RemoveAt(i);
+                    Console.WriteLine("Deleted animation");
+                }
+                else i++;
+            }
+        }
+
         static void Main()
         {
 
             window = new RenderWindow(new VideoMode(800, 600), "SnakeSFML");
             window.Closed += new EventHandler(OnClose);
 
-            textureW = new RenderTexture(20, 20); textureR = new RenderTexture(20, 20);
-            textureW.Clear(Color.White); textureR.Clear(Color.Red);
+            textureW = new RenderTexture(20, 20); textureR = new RenderTexture(20, 20); textureSR = new RenderTexture(10, 10);
+            textureW.Clear(Color.White); textureR.Clear(Color.Red); textureSR.Clear(Color.Red);
 
             pointToEat = new PointToEat.PointToEat(textureR.Texture, 60, 100);
 
             snakePartsList = new List<SnakeParticle>();
+            pointsAnims = new List<PointAnim>();
             direction = new SnakeDirection();
 
             Clock clock = new Clock();
 
             SnakeAddParticleToList(textureW.Texture, 20, 40);
             SnakeAddParticleToList(textureW.Texture, 20, 20);
-            
+
             window.KeyPressed += new EventHandler<KeyEventArgs>(OnKeyPressed);
 
             while (window.IsOpen)
@@ -204,7 +226,14 @@ namespace SnakeSFML
                     {
 
                         if (CheckPointPick())
+                        {
+
+                            pointsAnims.Add(new PointAnim(textureSR.Texture, pointToEat.GetX(), pointToEat.GetY()));
                             PointGained();
+
+
+                        }
+                        
                         MoveSnake(removeParticle);
                         clock.Restart();
                         canStillChangeDirection = true;
@@ -215,17 +244,18 @@ namespace SnakeSFML
                         {
                             Console.WriteLine("GAME OVER"); MainGameOn = false;
                         }
-                        
+
                     }
+                    DrawPointsAnim();
                     window.Display();
                 }
                 window.Clear(Color.Black);
-                
+
                 Text gameOver = new Text("GAME OVER", arialF, 16);
                 RenderTexture gameOverTexture = new RenderTexture(500, 300);
                 gameOver.Color = Color.White;
-                gameOver.Position = new Vector2f(window.Size.X/4,window.Size.Y/2);
-                gameOver.Draw(window,RenderStates.Default);
+                gameOver.Position = new Vector2f(window.Size.X / 4, window.Size.Y / 2);
+                gameOver.Draw(window, RenderStates.Default);
 
                 window.Display();
 
